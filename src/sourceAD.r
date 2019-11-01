10c323cf29d3bf075ea3fb68096038f4528bf56c
Samuel Sledzieski <samsledje@gmail.com>
2019-11-01 15:44:31 -0400

    Rmd notebook for annotating cell types from AD data

--- src/sourceAD.r
+++
@@ -1,5 +0,0 @@
-AD_SCE <- readRDS("data/Processed_10x_AD_data/AD_SCE_normalized.rds")
-AD_AN <- readRDS("data/Processed_10x_AD_data/AD_ACTIONet.rds")
-ACTIONetCellTypes <- readRDS("data/Processed_10x_AD_data/AD_ACTIONet_Types.rds")
-CellAssignCellTypes <- readRDS("data/Processed_10x_AD_data/AD_CellAssign_Types.rds")
-brainMarkers <- readRDS("data/markers/Brain_markers.RDS")$Celltype

41bae494a90f6cad0c3700fb5779666816559226
dhamanpreet-kaur <56942715+dhamanpreet-kaur@users.noreply.github.com>
2019-11-06 17:03:20 -0500

    Update dbn.R
    can take input files and find edges of network between time steps

--- src/dbn.R
+++ src/dbn.R
@@ -1,1 +1,59 @@
-# Test R file with a comment
+setwd("/Users/dkaur/Documents/Pseudotemporal_GRN_Learning")
+
+files <- c("data/DataMatrices/noAD_Oli.rds",
+           "data/DataMatrices/possibleAD_Oli.rds",
+           "data/DataMatrices/probableAD_Oli.rds",
+           "data/DataMatrices/definiteAD_Oli.rds"
+           )
+
+library(bnlearn)
+library(dplyr)
+
+mat_to_df <- function(matrix,time){
+  # matrix should have genes as rows, cells as columns
+  transposed <- t(matrix)
+  rownames(transposed) <- c()
+  rawdf <- as.data.frame(transposed)
+  rawdf[] <- lapply(rawdf, factor)
+  
+  # remove duplicate columns
+  df <- rawdf[, !duplicated(colnames(rawdf), fromLast = TRUE)] 
+  
+  # network learning requires > 1 possible value for each var
+  df <- select_if(df,function(x) return(nlevels(x)>1))
+  
+  # learn from gene expression at each time step to generate data
+  network <- hc(df)
+  simdf <- rbn(network, data = df, 10000)
+  colnames(simdf) <- paste(colnames(simdf), time, sep = "_")
+  return(simdf)
+}
+
+bl <- data.frame()
+bndf <- NULL
+prevcols <- c()
+
+for (i in (1:length(files))){
+  matrix <- readRDS(files[i])
+  newdf <- mat_to_df(matrix,paste("t",i,sep=""))
+  # only allow edges to future time points
+  bl <- rbind(bl,
+              expand.grid(colnames(newdf),prevcols),
+              expand.grid(colnames(newdf),colnames(newdf)))
+  prevcols <- c(prevcols,colnames(newdf))
+  if (is.null(bndf)){
+    bndf <- newdf
+  }
+  else{
+    bndf <- cbind(bndf,newdf)
+  }
+}
+
+# many different methods available for network learning
+network <- pc.stable(bndf,blacklist = bl)
+network <- gs(bndf, blacklist = bl)
+network <- iamb(bndf, blacklist = bl)
+
+# output edges of graph
+network[["arcs"]]
+

3b42ab49084c826c841b68579e3af02d9517e50c
Samuel Sledzieski <samsl@mit.edu>
2019-12-03 13:51:47 -0500

    gene selection with marker sets

--- src/dbn.R
+++ src/dbn.R
@@ -1,16 +1,52 @@
-#setwd("/home/samsl/Pseudotemporal-GRNs")
-setwd("D:/Drive/PhD/GitHub/Pseudotemporal_GRN_Learning")
+setwd("/home/samsl/Pseudotemporal-GRNs")
+#setwd("D:/Drive/PhD/GitHub/Pseudotemporal_GRN_Learning")
 
 require(bnlearn)
 require(dplyr)
 require(Rgraphviz)
 
+read.gmt <- function(file) {
+  if (!grepl("\\.gmt$", file)[1]) {
+    stop("Pathway information must be a .gmt file")
+  }
+  geneSetDB = readLines(file)
+  geneSetDB = strsplit(geneSetDB, "\t")
+  name = unlist(geneSetDB[1])
+  desc = strsplit(geneSetDB[2][[1]],"> ")[[1]][2]
+  geneSetDB = unlist(geneSetDB[-(1:2)])
+  geneSetDB = list("name"=name,"description"=desc,"genes"=geneSetDB)
+  return(geneSetDB)
+}
+
+# Select which data to load
 cell.type = "Mic"
 clusts <- readRDS(paste0("processed-data/",cell.type,"/clusters.rds"))
 df <- as.data.frame(readRDS(paste0("processed-data/",cell.type,"/top_gene_counts.rds")))
 K.clusters = 4
-N.genes = 25
 
+# Load gene lists for network generation
+marker.root <- "processed-data/markers/"
+kegg <- read.gmt(paste0(marker.root,"KEGG_AD.gmt"))
+adLib <- list("name"="library","description"="Library in AD SCE exeriment","genes"=colnames(df))
+
+blalock_incpt_dn <- read.gmt(paste0(marker.root,"BLALOCK_INCPT_AD_DN.gmt"))
+blalock_incpt_up <- read.gmt(paste0(marker.root,"BLALOCK_INCPT_AD_UP.gmt"))
+blalock_dn <- read.gmt(paste0(marker.root,"BLALOCK_AD_DN.gmt"))
+blalock_up <- read.gmt(paste0(marker.root,"BLALOCK_AD_UP.gmt"))
+
+gene.sets <- list("kegg"=kegg$genes,"incpt_dn"=blalock_incpt_dn$genes,"incpt_up"=blalock_incpt_up$genes,"dn"=blalock_dn$genes,"up"=blalock_up$genes)
+mat <- make_comb_mat(gene.sets, mode = "distinct")
+UpSet(mat,comb_order = order(-comb_size(mat)))
+
+# Select subset of genes
+gene.df <- df[,intersect(adLib$genes,kegg$genes)]
+
+# Write output for GRNVBEM
+pseudotime.sorted <- t(df)[,order(clusts$DiseaseScore)]
+write.csv(pseudotime.sorted, file=paste0("processed-data/",cell.type,"/GRNVBEM.csv"))
+
+# Divide cells into layers based on clusters
+N.genes = 25
 df <- df[,1:N.genes]
 clusts$ScoreClusts = as.numeric(clusts$DiseaseRange)
 
